5f086d0b5b7581860b972d4cf6040374961d1cb6
todx2 <todzone1@aol.com>
2019-05-06 11:18:04 -0500

    msg_from_tedros@dell

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+#may 6 2019  ted
+def index
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#ted may 6 2019
+root to: 'pages#index'
 
 
 end
